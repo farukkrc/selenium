0fd792edc40dd0b58ce5656665d0be36141419c8
pc <pc@hp>
2017-11-21 23:00:04 +0300

    Method Name Changed

--- src/myTest/JunitAssert.java
+++ src/myTest/JunitAssert.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 public class JunitAssert {
 	
 	@Test
-	public void testIt(){
+	public void testMethod(){
 		SeleniumGetHtml selenium = new SeleniumGetHtml();
 		
 		assertEquals(selenium.htmlData, HtmlData.data);
